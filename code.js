8f2682e85364248f77ba5da5387e2af5ee85f511
Wally <107082066+Waldorfio@users.noreply.github.com>
2022-09-13 15:57:16 +1000

    Add boilerplate factory functions

--- code.js
+++ code.js
@@ -1,6 +1,21 @@
 console.clear();
 console.log('-----');
 
-function node() {     // Node Factory 
+function node(data, child) {   // Node Factory 
+  return {
+    data,
+    child,
+  }
 }
 
+function tree(root) {  // Tree Factory
+  return {
+      root,           // Root attribute
+  }
+}
+
+function buildTree(array) {   // returns the level-0 root node.
+  return root
+}
+
+test = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
