4d3e5a649964a388572b90058f89a5ec94d77164
Wally <107082066+Waldorfio@users.noreply.github.com>
2022-09-13 15:59:47 +1000

    Add expected output

--- code.js
+++ code.js
@@ -1,21 +1,37 @@
-console.clear();
-console.log('-----');
+// test = [1, 2, 3, 4, 5, 6, 7];
 
-function node(data, child) {   // Node Factory 
-  return {
-    data,
-    child,
-  }
-}
+// Assuming the following output format:
+//       4
+//   2       6
+// 1   3   5   7
+// as an object literal
 
-function tree(root) {  // Tree Factory
-  return {
-      root,           // Root attribute
-  }
-}
-
-function buildTree(array) {   // returns the level-0 root node.
-  return root
-}
-
-test = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+// tree = {
+//     data: 4,
+//     left: {
+//         data: 2,
+//         left: {
+//             data: 1,
+//             left: null,
+//             right: null
+//         },
+//         right: {
+//             data: 3,
+//             left: null,
+//             right: null
+//         }
+//     },
+//     right: {
+//         data: 6,
+//         left: {
+//             data: 5,
+//             left: null,
+//             right: null
+//         },
+//         right: {
+//             data: 7,
+//             left: null,
+//             right: null
+//         }
+//     }
+// };
