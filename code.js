fc4205b5d62a2aeb3241da61c35273180479eae6
Wally <107082066+Waldorfio@users.noreply.github.com>
2022-09-14 12:57:00 +1000

    Add first attempt to solving recursion

--- code.js
+++ code.js
@@ -1,25 +1,48 @@
 console.clear();
 console.log('-----');
 
-function node(data, child) {   // Node Factory 
+function node(data, left, right) {   // Node Factory 
   return {
     data,
-    child,
+    left,
+    right,
   }
 }
+exampleNode = node(4, null, null);
 
-function tree(root) {  // Tree Factory
-  return {
-      root,           // Root attribute
+// function tree(root) {  // Tree Factory
+//   return {
+//       root,           // Root attribute
+//   }
+// }
+
+function buildTree(arr, tree, dir) {   // returns the level-0 root node.
+  console.log('running buildTree...');
+  // Base Case
+  if (arr.length < 1) {
+    return
   }
+  
+  midIdx = (arr.length-1)/2;
+  midNode = node(arr[midIdx], {}, {});
+
+  if (dir=='initial') {tree = midNode};
+  if (dir=='left') {tree.left = midNode};
+  if (dir=='right') {tree.right = midNode};
+
+  lhs = arr.splice(0, midIdx);
+  rhs = arr.splice(1, arr.length);
+
+  buildTree(lhs, tree.left, 'left');
+  buildTree(rhs, tree.right, 'right');
+  console.log(tree);
+  return
+
 }
 
-function buildTree(array) {   // returns the level-0 root node.
-  return root
-}
+test = [1, 2, 3, 4, 5, 6, 7];
 
-
-// test = [1, 2, 3, 4, 5, 6, 7];
+buildTree(test, {}, 'initial');
 
 // Assuming the following output format:
 //       4
@@ -27,32 +50,33 @@ function buildTree(array) {   // returns the level-0 root node.
 // 1   3   5   7
 // as an object literal
 
-// tree = {
-//     data: 4,
-//     left: {
-//         data: 2,
-//         left: {
-//             data: 1,
-//             left: null,
-//             right: null
-//         },
-//         right: {
-//             data: 3,
-//             left: null,
-//             right: null
-//         }
-//     },
-//     right: {
-//         data: 6,
-//         left: {
-//             data: 5,
-//             left: null,
-//             right: null
-//         },
-//         right: {
-//             data: 7,
-//             left: null,
-//             right: null
-//         }
-//     }
-// };
+
+tree2 = {
+    data: 4,
+    left: {
+        data: 2,
+        left: {
+            data: 1,
+            left: null,
+            right: null
+        },
+        right: {
+            data: 3,
+            left: null,
+            right: null
+        }
+    },
+    right: {
+        data: 6,
+        left: {
+            data: 5,
+            left: null,
+            right: null
+        },
+        right: {
+            data: 7,
+            left: null,
+            right: null
+        }
+    }
+};
